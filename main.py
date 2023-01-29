1867b36381636c20a83a9169810483245530cd1f
sunandbean <sunbin.vincent.kim@gmail.com>
2023-01-29 21:00:27 +0900

    finish to refactor the code

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from config import NOTION_TOKEN, DATABASE_ID
+from config import NOTION_TOKEN, DATABASE_ID, NAVER_ID, NAVER_PW, CUSTOMERS
 from model import Model
 from controller import Controller
+from viewer import Viewer
 
 if __name__ == "__main__":
     model = Model(NOTION_TOKEN, DATABASE_ID)
     controller = Controller(model.get_dataframe())
-    
+    viewer = Viewer(NAVER_ID, NAVER_PW, CUSTOMERS)
+
+    viewer.add_content(controller.filter_data(1)) # yesterday
+    viewer.add_content(controller.filter_data(7)) # a week ago
+    viewer.add_content(controller.filter_data(30)) # a month ago
+
+    viewer.send_mail()
+
