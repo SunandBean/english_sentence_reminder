961d83098561aa99da96f89093f2626f412f85d9
sunandbean <sunbin.vincent.kim@gmail.com>
2023-01-29 20:29:15 +0900

    add controller in main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from config import NOTION_TOKEN, DATABASE_ID
 from model import Model
+from controller import Controller
 
 if __name__ == "__main__":
     model = Model(NOTION_TOKEN, DATABASE_ID)
+    controller = Controller(model.get_dataframe())
+    
