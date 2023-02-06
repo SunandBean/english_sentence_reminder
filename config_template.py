5683ff8761f249de8c195bb58434bcf6a7116628
sunandbean <sunbin.vincent.kim@gmail.com>
2023-02-06 22:11:42 +0900

    add gspread related keys

--- config_template.py
+++ config_template.py
@@ -3,3 +3,5 @@ DATABASE_ID = "your database id"
 NAVER_ID = "your naver id"
 NAVER_PW = "your naver pw"
 CUSTOMERS = ["your customer1", "your customer2"]
+GSPREAD_KEY = "gspread_key_example.json"
+GSPREAD_SHEET_URL = "https://docs.google.com/spreadsheets/"
